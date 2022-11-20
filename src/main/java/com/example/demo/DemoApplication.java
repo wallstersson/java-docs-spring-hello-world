46781fa9ad55897352baa66fbc0c24e59e7a8495
Marcus Wallstersson <marcus.wallstersson@microsoft.com>
2022-11-20 19:52:19 +0000

    Added app insights custom event

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,26 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-// import com.microsoft.applicationinsights.attach.ApplicationInsights
-// import com.microsoft.applicationinsights.TelemetryClient;
+
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
+import com.microsoft.applicationinsights.TelemetryClient;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 		
-	// private TelemetryClient telemetry = new TelemetryClient();
+private static TelemetryClient telemetry = new TelemetryClient();
 
 	public static void main(String[] args) {
-		// ApplicationInsights.attach();
-		// telemetry.getContext().getUser().setId("1234");
-		// telemetry.getContext().getDevice().setId("12345");
-
+		ApplicationInsights.attach();
+		telemetry = new TelemetryClient();
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@RequestMapping("/")
 	String sayHello() {
-		// telemetry.trackEvent("Requested Hello Azure");
-		return "Hello Azure!";
+		telemetry.trackEvent("Requested Hello Azure 1");
+		return "Hello Azure 1!";
 	}
 }
